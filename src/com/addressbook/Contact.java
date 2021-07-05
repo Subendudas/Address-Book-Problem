94646117840c2e5886724d01ba87675554518b6b
Subendu Das <31320094+Subendudas@users.noreply.github.com>
2021-07-05 18:07:46 +0530

    Added: Ability To Add New Contacts.

---
+++ src/com/addressbook/Contact.java
@@ -0,0 +1,28 @@
+package com.addressbook;
+
+public class Contact {
+	////Initialize all the parameters
+		private String firstName;
+		private String lastName;
+		private String address;
+		private String city;
+		private String state;
+		private String zip;
+		private String phone;
+		private String email;
+		// call by Constructor
+		public Contact(String firstName, String lastName, String Address, String city, String state, String zip, String phone,
+				String email) 
+		{
+			// Using Super key
+			super();
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.city = city;
+			this.state = state;
+			this.zip = zip;
+			this.phone = phone;
+			this.email = email;
+		}
+
+}
