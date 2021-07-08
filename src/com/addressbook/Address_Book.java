f5422073affd12ebe27652f521148b7482d23fef
Subendu Das <31320094+Subendudas@users.noreply.github.com>
2021-07-08 16:32:29 +0530

    Added: Delete Contact Using Person's Name.

--- src/com/addressbook/Address_Book.java
+++ src/com/addressbook/Address_Book.java
@@ -1,54 +1,139 @@
 package com.addressbook;
-import java.util.List;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Scanner;
-public class Address_Book
-{
-	public static void main(String[] args) {
-		Address_Book obj = new Address_Book(); 
-	System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
-		System.out.println("Create Contact");
-		addContact();
-		obj.editContact();
-	}
-	static Scanner sc = new Scanner(System.in);
-	static List<Contact> list = new LinkedList<Contact>();
-	
-	public static void addContact() 
-	{
-		System.out.print("Enter Firts Name : ");
-		String firstName = sc.nextLine();
-		System.out.print("Enter your Last Name : ");
-		String lastName = sc.nextLine();
-		System.out.print("Enter your Address : ");
-		String address = sc.nextLine();
-		System.out.print("Enter your City : ");
-		String city = sc.nextLine();
-		System.out.print("Enter your State : ");
-		String state = sc.nextLine();
-		System.out.print("Enter your Zip code : ");
-		String zip = sc.nextLine();
-		System.out.print("Enter your PhoneNo : ");
-		String phone = sc.nextLine();
-		System.out.print("Enter your Email : ");
-		String email = sc.nextLine();
-		Contact obj = new Contact(firstName, lastName, address, city, state, zip, phone, email);
-		list.add(obj);
-	}
 
-	public static void editContact() {
-		// Scanner sc = new Scanner(System.in);
-		System.out.println("Enter First Name which you want to edit: ");
-		String firstName = sc.nextLine();
-		for (int i = 0; i < list.size(); i++) {
-			if (( list.get(i)).getFirstName().equalsIgnoreCase(firstName)) {
-				list.remove(i);
-				addContact();
-				System.out.println("Data Added Successfully.");
-			} else {
-				System.out.println("No data found in Address Book");
+public class Address_Book extends Contact 
+{
+	Scanner sc = new Scanner(System.in);
+	private ArrayList<Contact> contactList = new ArrayList<Contact>();
+	
+	public Address_Book(String firstname, String lastname, String address, String city, String state, String zip,
+			String phone, String email) {
+		super(firstname, lastname, address, city, state, zip, phone, email);
+	}
+	public Address_Book() {
+		super();
+	}
+	public void insertContact(Address_Book contactDetailObject) {
+		contactList.add(contactDetailObject);
+	}
+	
+	public void editContact(String nameToEdit) {
+		for (Contact editContact : contactList) {
+			if (editContact.getFirstName().equals(nameToEdit)) {
+				System.out.println("Edit Firstname : ");
+				String firstname = sc.nextLine();
+				editContact.setFirstName(firstname);
+				System.out.println("Edit Lastname : ");
+				String lastname = sc.nextLine();
+				editContact.setLastName(lastname);
+				System.out.println("Edit Address : ");
+				String address = sc.nextLine();
+				editContact.setAddress(address);
+				System.out.println("Edit City : ");
+				String city = sc.nextLine();
+				editContact.setCity(city);
+				System.out.println("Edit State : ");
+				String state = sc.nextLine();
+				editContact.setState(state);
+				System.out.println("Edit Zip : ");
+				String zip = sc.nextLine();
+				editContact.setZip(zip);
+				System.out.println("Edit PhoneNumber : ");
+				String phonenumber = sc.nextLine();
+				sc.nextLine();
+				editContact.setPhoneNo(phonenumber);
+				System.out.println("Edit Email : ");
+				String email = sc.nextLine();
+				editContact.setEmail(email);
 			}
 		}
 	}
-}
+	
+	public void delete(String nameToDelete) {
+		int i = 0;
+		for (i = 0; i < contactList.size(); i++) {
+			if (contactList.get(i).firstName.equals(nameToDelete)) {
+				contactList.remove(i);
+				System.out.println("Contact is Deleted");
+			} else {
+				System.out.println("Contact not found!");
+			}
+		}
+	}
+	
+	public void printContactDetails() {
+		for (Contact getInfo : contactList) {
+			System.out.println("Firstname : " + getInfo.getFirstName());
+			System.out.println("Lastname : " + getInfo.getLastName());
+			System.out.println("Address : " + getInfo.getAddress());
+			System.out.println("City : " + getInfo.getCity());
+			System.out.println("State : " + getInfo.getState());
+			System.out.println("Zip : " + getInfo.getZip());
+			System.out.println("PhoneNumber : " + getInfo.getPhoneNo());
+			System.out.println("Email : " + getInfo.getEmail());
+		}
+	}
+	public static void main(String args[]) 
+	{
+		Scanner scan = new Scanner(System.in);
+		Address_Book Obj = new Address_Book();
+		String choice = "y";
+		System.out.println("Welcome to Address Book Program!");
+		while (true) {
+			System.out.println("Enter 1 to add, 2 to edit 3 to delete 4 to view 5 to exit");
+			System.out.println("Enter choice : ");
+			int switchChoice = scan.nextInt();
+			scan.nextLine();
+			
+			switch (switchChoice) {
+			case 1:
+				while (choice.equals("y")) {
+					System.out.println("Firstname : ");
+					String firstname = scan.nextLine();
+					System.out.println("Lastname : ");
+					String lastname = scan.nextLine();
+					System.out.println("Address : ");
+					String address = scan.nextLine();
+					System.out.println("City : ");
+					String city = scan.nextLine();
+					System.out.println("State : ");
+					String state = scan.nextLine();
+					System.out.println("Zip : ");
+					String zip = scan.nextLine();
+					System.out.println("PhoneNumber : ");
+					String phonenumber = scan.nextLine();
+					System.out.println("Email : ");
+					scan.nextLine();
+					String email = scan.nextLine();
+					System.out.println("Do you want add more contact : ");
+					choice = scan.nextLine();
 
+					Address_Book contact = new Address_Book(firstname, lastname, address, city, state, zip, phonenumber,
+							email);
+					Obj.insertContact(contact);
+				}
+				break;
+			case 2:
+				System.out.println("Enter name of contact to Edit : ");
+				String nameToEdit = scan.nextLine();
+				Obj.editContact(nameToEdit);
+				break;
+			case 3:
+				System.out.println("Enter name of contact to Delete : ");
+				String deleteCon = scan.nextLine();
+				Obj.delete(deleteCon);
+				break;
+			case 4:
+				System.out.println("Contact List : ");
+				Obj.printContactDetails();
+				break;
+			default:
+				System.exit(0);
+				scan.close();
+			}
+		}
+
+	}
+
+}
--- src/com/addressbook/Contact.java
+++ src/com/addressbook/Contact.java
@@ -1,8 +1,12 @@
 package com.addressbook;
 
+import java.util.Iterator;
+
+import javax.xml.crypto.Data;
+
 public class Contact {
 	////Initialize all the parameters
-		private String firstName;
+		protected String firstName;
 		private String lastName;
 		private String address;
 		private String city;
@@ -24,9 +28,60 @@ public class Contact {
 			this.phone = phone;
 			this.email = email;
 		}
+		public Contact() {
+			// TODO Auto-generated constructor stub
+		}
 		public String getFirstName() {
-			// TODO Auto-generated method stub
 			return firstName;
 		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		public String getState() {
+			return state;
+		}
+		public void setState(String state) {
+			this.state = state;
+		}
+		public String getZip() {
+			return zip;
+		}
+		public void setZip(String zip) {
+			this.zip = zip;
+		}
+		public String getPhoneNo() {
+			return phone;
+		}
+		public void setPhoneNo(String phone) {
+			this.phone = phone;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public static Iterator<Data> iterator() {
+			return null;
+		}
 		
-}
+		}
+		
